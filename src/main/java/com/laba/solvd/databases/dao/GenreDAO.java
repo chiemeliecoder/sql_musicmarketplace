fe2a06be9493aa00a88611d2c04d10a712fa4cd1
chiemeliecode <ezeokeke.c@northeastern.edu>
2023-06-13 09:19:54 -0400

    My Model Updated

--- src/main/java/com/laba/solvd/databases/dao/GenreDAO.java
+++ src/main/java/com/laba/solvd/databases/dao/GenreDAO.java
@@ -1,5 +1,8 @@
 package com.laba.solvd.databases.dao;
 
+import com.laba.solvd.databases.configurations.ConnectionPool;
+import com.laba.solvd.databases.interfacedao.IGenreDAO;
+import com.laba.solvd.databases.model.Albums;
 import com.laba.solvd.databases.model.Artists;
 import com.laba.solvd.databases.model.Genre;
 import java.io.FileInputStream;
@@ -11,9 +14,34 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
-public class GenreDAO {
+public class GenreDAO implements IGenreDAO {
+
+  private static final ConnectionPool CONNECTION_POOL = ConnectionPool.getInstance();
+
+  @Override
+  public void createGenre(Genre genre) {
+    Connection connection = CONNECTION_POOL.getConnectionFromPool();
+    try(PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO genre (id, name) VALUES (?, ?)",
+        Statement.RETURN_GENERATED_KEYS)){
+      preparedStatement.setInt(1, genre.getId());
+      preparedStatement.setString(2, genre.getGenreName());
+
+      preparedStatement.executeUpdate();
+      ResultSet resultSet = preparedStatement.getGeneratedKeys();
+      while (resultSet.next()){}
+
+    }catch(SQLException e){
+      throw new RuntimeException("unable to create album", e);
+    }finally {
+      CONNECTION_POOL.releaseConnectionToPool(connection);
+    }
+
+  }
 
   public Genre getGenreById(int id) throws SQLException {
 
@@ -45,6 +73,30 @@ public class GenreDAO {
 
     return genre;
 
+  }
+
+  @Override
+  public List<Genre> getAllGenres() {
+    List<Genre> genre = new ArrayList<>();
+
+    Connection connection = CONNECTION_POOL.getConnectionFromPool();
+    try(PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM GENRE")){
+      ResultSet resultSet = preparedStatement.executeQuery();
+      while (resultSet.next()){
+        Genre genre1 = new Genre();
+        genre1.setId(resultSet.getInt("id"));
+        genre1.setGenreName(resultSet.getString("name"));
+
+
+
+        genre.add(genre1);
+      }
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    }finally{
+      CONNECTION_POOL.releaseConnectionToPool(connection);
+    }
+    return genre;
   }
 
   public static void main(String args[]) throws SQLException {
