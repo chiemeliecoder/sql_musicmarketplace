a4c048f44e729822d88b4df11ef20d4c4d39c9de
Chiemelie Ezeokeke <77821039+chiemeliecoder@users.noreply.github.com>
2023-06-13 12:30:44 -0400

    Delete target/classes directory

--- target/classes/playlist_track.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM musicmarketplace.playlist_track;
-INSERT INTO playlist_track (id, idplaylist, idtrack) VALUES (1, 1, 1);
-DELETE FROM playlist_track WHERE (id = 1);
-
--- Retrieve the playlist with distinct tracks
-SELECT musicmarketplace.playlist_track.id, COUNT(DISTINCT musicmarketplace.playlist_track.idtrack) AS distinct_track_count
-FROM musicmarketplace.playlist_track
-GROUP BY musicmarketplace.playlist_track.id
-HAVING COUNT(DISTINCT musicmarketplace.playlist_track.idtrack) > 0;
