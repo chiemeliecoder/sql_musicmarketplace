4750fa1a02a70893e67a2c30a4e7e6c4274eb6c3
chiemeliecode <ezeokeke.c@northeastern.edu>
2023-06-14 06:01:59 -0400

    xml to resources

--- src/main/resources/user.sql
+++ src/main/resources/user.sql
@@ -103,4 +103,111 @@ FROM musicmarketplace.artists
 JOIN musicmarketplace.album ON musicmarketplace.artists.id = musicmarketplace.album.id
 GROUP BY musicmarketplace.artists.id
 HAVING COUNT(musicmarketplace.album.id) >= 1;
+================================================================================================
+SELECT * FROM musicmarketplace.artist_achievements;
+INSERT INTO artist_achievements (id, artistid, title, date) VALUES (1, 1, 'billbord 100', '2014-10-29');
+UPDATE artist_achievements SET title = 'Grammy Award' WHERE (id = 1);
 
+
+-- Determine the artists who have achieved at least 1 achievement
+SELECT musicmarketplace.artists.id, musicmarketplace.artists.name, COUNT(musicmarketplace.artist_achievements.id) AS achievement_count
+FROM musicmarketplace.artists
+JOIN musicmarketplace.artist_achievements ON musicmarketplace.artists.id = musicmarketplace.artist_achievements.artistid
+GROUP BY musicmarketplace.artists.id, musicmarketplace.artists.name
+HAVING achievement_count >= 1;
+================================================================================================
+SELECT * FROM musicmarketplace.artist_genre;
+INSERT INTO artist_genre (id, artistid, genreid) VALUES (1, 1, 1);
+DELETE FROM artist_genre WHERE (id = 1);
+ALTER TABLE artistgenre RENAME TO Artist_MusicGenre;
+
+SELECT * 
+FROM artist_genre
+JOIN artists ON artist_genre.artistid = artists.id
+JOIN genre ON artist_genre.genreid = genre.id;
+================================================================================================
+SELECT * FROM musicmarketplace.artists;
+INSERT INTO artists (id, name) VALUES (1, 'Taylor Swift');
+UPDATE artists SET name = 'Alison Swift' WHERE (id = 1);
+DELETE FROM artists WHERE (id = 1);
+
+-- Outer Join
+SELECT *
+FROM musicmarketplace.artists
+LEFT OUTER JOIN musicmarketplace.artist_genre ON musicmarketplace.artists.id = musicmarketplace.artist_genre.artistid;
+===============================================================================================
+SELECT * FROM musicmarketplace.genre;
+INSERT INTO genre (id, name) VALUES (1, 'Pop');
+UPDATE genre SET name = 'Pop/Rock' WHERE (id = 1);
+DELETE FROM genre WHERE (id = 1);
+ALTER TABLE genre DROP COLUMN name;
+ALTER TABLE genre ADD COLUMN name VARCHAR(255);
+
+-- Find the total number of track in each genre
+SELECT musicmarketplace.genre.id, musicmarketplace.genre.name, COUNT(musicmarketplace.track.id) AS track_count
+FROM musicmarketplace.genre
+LEFT JOIN musicmarketplace.artist_genre ON musicmarketplace.genre.id = musicmarketplace.artist_genre.genreid
+LEFT JOIN musicmarketplace.artists ON musicmarketplace.artist_genre.artistid = musicmarketplace.artists.id
+LEFT JOIN musicmarketplace.album ON musicmarketplace.artists.id = musicmarketplace.album.id
+LEFT JOIN musicmarketplace.track ON musicmarketplace.album.id = musicmarketplace.track.albumid
+GROUP BY musicmarketplace.genre.id, musicmarketplace.genre.name;
+===============================================================================================
+SELECT * FROM musicmarketplace.playlist_track;
+INSERT INTO playlist_track (id, idplaylist, idtrack) VALUES (1, 1, 1);
+DELETE FROM playlist_track WHERE (id = 1);
+
+-- Retrieve the playlist with distinct track
+SELECT musicmarketplace.playlist_track.id, COUNT(DISTINCT musicmarketplace.playlist_track.idtrack) AS distinct_track_count
+FROM musicmarketplace.playlist_track
+GROUP BY musicmarketplace.playlist_track.id
+HAVING COUNT(DISTINCT musicmarketplace.playlist_track.idtrack) > 0;
+================================================================================================
+SELECT * FROM musicmarketplace.playlist;
+INSERT INTO playlist (id, name, userid) VALUES (1, 'My Favorites', 1);
+UPDATE playlist SET name = 'Big Hits' WHERE (id = 1);
+DELETE FROM playlist WHERE (id = 1);
+-- Right Join
+SELECT *
+FROM musicmarketplace.playlist
+RIGHT JOIN musicmarketplace.track ON musicmarketplace.playlist.id = musicmarketplace.track.id;
+
+-- Determine the number of playlist created by each user:
+SELECT musicmarketplace.playlist.userid, COUNT(musicmarketplace.playlist.id) AS playlist_count
+FROM musicmarketplace.playlist
+GROUP BY musicmarketplace.playlist.userid;
+================================================================================================
+SELECT * FROM musicmarketplace.purchase;
+INSERT INTO purchase (id, userid, trackid, date, price) VALUES (1, 1, 1, '2023-05-25', 9.99);
+UPDATE purchase SET price = 7.99 WHERE (id = 1);
+DELETE FROM purchase WHERE (id = 1);
+
+-- Calculate the total price of purchase for each user
+SELECT userid, SUM(musicmarketplace.purchase.price) AS total_price
+FROM musicmarketplace.purchase
+GROUP BY musicmarketplace.purchase.userid;
+
+-- Find users who have made at most 3 purchase with a total price less than $100
+SELECT musicmarketplace.purchase.userid, COUNT(musicmarketplace.purchase.id) AS num_purchases, SUM(musicmarketplace.purchase.price) AS total_price
+FROM musicmarketplace.purchase
+GROUP BY musicmarketplace.purchase.userid
+HAVING COUNT(musicmarketplace.purchase.id) <= 3 AND SUM(musicmarketplace.purchase.price) < 100;
+================================================================================================
+SELECT * FROM musicmarketplace.review;
+INSERT INTO review (id, userid, albumid, rating, comment) VALUES (1, 1, 1, 5.0, 'Great album!');
+UPDATE review SET rating = 4.0 WHERE (id = 1);
+DELETE FROM review WHERE (id = 1);
+
+-- Find genres with an average rating greater than 3.5
+SELECT musicmarketplace.genre.id, musicmarketplace.genre.name, AVG(musicmarketplace.review.rating) AS avg_rating
+FROM musicmarketplace.genre
+JOIN musicmarketplace.artist_genre ON musicmarketplace.genre.id = musicmarketplace.artist_genre.genreid
+JOIN musicmarketplace.artists ON musicmarketplace.artist_genre.artistid = musicmarketplace.artists.id
+JOIN musicmarketplace.album ON musicmarketplace.artists.id = musicmarketplace.album.artistid
+JOIN musicmarketplace.review ON musicmarketplace.album.id = musicmarketplace.review.albumid
+GROUP BY musicmarketplace.genre.id, musicmarketplace.genre.name
+HAVING AVG(musicmarketplace.review.rating) > 3.5;
+
+-- Calculate the highest and lowest rating for each album
+SELECT musicmarketplace.review.albumid, MAX(musicmarketplace.review.rating) AS highest_rating, MIN(musicmarketplace.review.rating) AS lowest_rating
+FROM musicmarketplace.review
+GROUP BY musicmarketplace.review.albumid;
